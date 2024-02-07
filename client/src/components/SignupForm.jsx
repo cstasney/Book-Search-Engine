98487dbe46b6bf082b42c398ae2926abff35a36c
cstasney <cstasney07@gmail.com>
2024-02-07 18:18:07 -0500

    replaced loginUser with login mutation

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 import { useMutation } from '@apollo/client';
