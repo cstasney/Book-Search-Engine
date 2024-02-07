a34918a9db328a606306d886ea3ea31ef7556716
cstasney <cstasney07@gmail.com>
2024-02-07 15:45:58 -0500

    typeDefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,12 @@
+// const { User } = require('../models');
+// const { signToken, AuthenticationError } = require('../utils/auth');
+
+// cosnt resolvers = {
+//     Query: {
+//         me: async (parents, args, context) =>{
+//             if (context.user) {
+//                 const userData = await User.findOne({ _id: context.user.id })
+//             }
+//         }
+//     }
+// }
